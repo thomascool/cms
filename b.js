b03c578dacce68f28eb791d861f2a4fae039c57e
thomascool <thomas@Wing-Nang-Kus-MacBook-Pro.local>
2013-03-23 21:59:56 -0700

    testing

--- b.js
+++ b.js
@@ -1,6 +1,8 @@
 var http = require('http');
 var ts = new Date().getTime();
 var parser = require("node-xml2json");
+var  _ = require("underscore");
+
 
 function wget (host, path, https, callback) {
   var options = {
@@ -35,7 +37,6 @@ function wget (host, path, https, callback) {
  console.log(atom);
  });
 
- */
 
 
 wget('www.nasdaq.com', '/aspx/NLS/NLSHandler.ashx?msg=Last&Symbol=AAPL&QESymbol=AAPL&ts='+ts, false, function (atom) {
@@ -47,4 +48,17 @@ wget('www.nasdaq.com', '/aspx/NLS/NLSHandler.ashx?msg=MIN&Symbol=UVXY&QESymbol=U
   console.log(  JSON.stringify(parser.parser(atom) ,null," ") );
 });
 
+
+ wget('chartapi.finance.yahoo.com', '/instrument/1.0/aapl/chartdata;type=quote;range=5d/json?finance_charts_json_callback=?', false, function (atom) {
+ var response = eval(atom.replace('00"\n }' , '00},'));
+ console.log(atom);
+ });
+
+// http://http://download.finance.yahoo.com/d/quotes.csv?s=%40%5EDJI,GOOG&f=nsl1op&e=.csv/d/quotes.csv?s=%40%5EDJI,GOOG&f=nsl1op&e=.csv
+// http://chartapi.finance.yahoo.com/instrument/1.0/GOOG/chartdata;type=quote;range=1d/csv
+ */
+
+wget('chartapi.finance.yahoo.com', '/instrument/1.0/UVXY/chartdata;type=quote;range=5d/csv', false, function (atom) {
+  console.log(_.last(_.first(atom.split('\n'),20),7));
+});
 
