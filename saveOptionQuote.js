35a5d09e1b182ea2ccea705a52e6eb6c81af6858
thomas cool <thomas.cool@gmail.com>
2015-02-24 01:21:22 -0800

    code updates

--- saveOptionQuote.js
+++ saveOptionQuote.js
@@ -1,3 +1,22 @@
-/**
- * Created by thomas on 2/23/15.
- */
+var async = require('async'),
+  _ = require('underscore');
+
+var oQuote = require('./lib/optionQuote');
+
+//_.map(['AAPL','$SPX.X','UVXY','SVXY'], function(item){
+
+/*
+_.map(['AAPL'], function(item){
+  return oQuote.optionQuote(item);
+});
+
+*/
+
+async.eachSeries(['AAPL','FB'], function(item, ecb) {
+  oQuote.optionQuote(item, function() {
+    ecb();
+  });
+}, function(err) {
+  if (err)
+    console.log(err);
+});
